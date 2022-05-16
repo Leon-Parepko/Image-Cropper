4aaa849cf6ba60bb806bac588e112a4a4594366a
Leon-Parepko <l.parepko@innopolis.university>
2022-05-16 22:01:13 +0600

    Add split functional, REMINDER: separate Main into gui and functional part

--- Main.py
+++ Main.py
@@ -50,33 +50,62 @@ def resize_to_preview(img, max_size):
     return resized
 
 
+def split_img(img, split):
+
+    out_arr = []
+    block_dim = [img.shape[0] // split[0], img.shape[1] // split[1]]
+
+    for h in range(0, split[0]):
+        h_slice = img[h * block_dim[0]:(h + 1) * block_dim[0]]
+        h_slice = cv2.rotate(h_slice, cv2.ROTATE_90_CLOCKWISE)
+        for w in range(0, split[1]):
+            v_slice = h_slice[w * block_dim[1]:(w + 1) * block_dim[1]]
+            v_slice = cv2.rotate(v_slice, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            out_arr.append(v_slice)
+
+    return out_arr
 
 
 
-def process(cwd, border, color_rgb):
+
+def process(cwd, border, color_rgb, split):
+
     if not os.path.exists(cwd):
         write_to_text_field("There is no such directory!", type="e")
         return 0
 
-
-
     for file in os.listdir(cwd):
         if file.endswith(".jpg") or file.endswith(".png"):
+            # Read file
             file_path = os.path.join(cwd, file)
-
             img = cv2.imread(file_path, 1)
-            img_b = resize(img, 100 + border)
 
-            back = np.full(img_b.shape, color_rgb, dtype=np.uint8)
-            h, w = img_b.shape[:2]
-            h1, w1 = img.shape[:2]
-
-            cx, cy = (h - h1) // 2, (w - w1) // 2
-
-            back[cx:h1+cx, cy:w1+cy] = img
-
-            cv2.imwrite(os.path.join(cwd, f'(res)_{file}'), back)
+            # Write Processing msg
             write_to_text_field(f"Processing {file_path}", type="i")
+
+            # Split img into the blocks
+            splitted_img = split_img(img, split)
+
+            # Process
+            iter = 1
+            for block in splitted_img:
+                img_b = resize(block, 100 + border)
+
+                back = np.full(img_b.shape, color_rgb, dtype=np.uint8)
+                h, w = img_b.shape[:2]
+                h1, w1 = block.shape[:2]
+
+                cx, cy = (h - h1) // 2, (w - w1) // 2
+
+                back[cx:h1+cx, cy:w1+cy] = block
+
+                file_splitted = file.split(".")
+                if len(splitted_img) == 1:
+                    cv2.imwrite(os.path.join(cwd, f'{file_splitted[0]}_(res).{file_splitted[1]}'), back)
+                else:
+                    cv2.imwrite(os.path.join(cwd, f'{file_splitted[0]}_(res_{iter}).{file_splitted[1]}'), back)
+
+                iter += 1
 
 
 def preprocess(border):
@@ -105,7 +134,7 @@ def set_rgb_sliders(content):
         red_slider.set(content[0])
         green_slider.set(content[1])
         blue_slider.set(content[2])
-        
+
 
 
 def chose_dir():
@@ -179,7 +208,7 @@ preview = tk.Label(window, image=imgtk)
 
 text_field = tk.Text(window, height=10, width=45)
 
-confirm_button = tk.Button(window, text='Process', width=25, command=lambda: process(dir_entry.get(), border_slider.get(), [blue_slider.get(), green_slider.get(), red_slider.get()]))
+confirm_button = tk.Button(window, text='Process', width=25, command=lambda: process(dir_entry.get(), border_slider.get(), [blue_slider.get(), green_slider.get(), red_slider.get()], [split_H_slider.get(), split_V_slider.get()]))
 
 
 
